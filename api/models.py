7237f25892377eaa9e539ff40ee7b3211c4e6d69
Noah Kim <noahbkim@gmail.com>
2016-12-01 14:09:32 -0500

    Started working on the models. Consider border objects for actual timeline?

--- api/models.py
+++ api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """A profile model that contains blocks data."""
+
+    user = models.ForeignKey(User)
+
+
+class Category(models.Model):
+    """A category of activity."""
+
+    name = models.CharField(max_length=30)
+
+
+class Tag(models.Model):
+    """A custom tag that describes an activity."""
+
+    name = models.CharField(max_length=30)
+    creator = models.ForeignKey(Profile, related_name="tags")
+
+
+class Activity(models.Model):
+    """An activity container."""
+
+    name = models.CharField(max_length=60)
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag)
+    color = models.CharField(max_length=7)
+    creator = models.ForeignKey(Profile, related_name="activites")
+
